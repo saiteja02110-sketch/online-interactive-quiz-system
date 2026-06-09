a73ae5fbcd95f2f83e642a4b8d2aa90e1535a5ee
saiteja02110-sketch <saiteja02110@gmail.com>
2026-06-09 09:57:02 +0530

    Create Participant class with basic attributes

---
+++ Participant.java
@@ -0,0 +1,12 @@
+package com.quizsystem.model;
+
+public class Participant {
+
+    private String name;
+    private int score;
+    private double totalTime;
+
+    public Participant() {
+    }
+
+}
